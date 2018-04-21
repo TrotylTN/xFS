c75e01f36eff4c8b642809000cd4d97a1efd3def
zhou0745 <zhou0745@umn.edu>
2018-04-21 12:49:08 -0500

    completed local framework for client side

---
+++ xFS_Server.py
@@ -0,0 +1,13 @@
+# CSCI 5105 Project 3
+# Team Member: Tiannan Zhou, Xuan Bi
+# Written in Python 3
+
+import sys, threading, os
+from socket import *
+from datetime import datetime
+from argparse import ArgumentParser
+
+def main():
+    # main function
+
+main()
