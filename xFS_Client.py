359c6f0be81a6acf9d8afe793d84d18aa6133379
zhou0745 <zhou0745@umn.edu>
2018-04-21 21:59:57 -0500

    Added a fixed packet length

--- xFS_Client.py
+++ xFS_Client.py
@@ -7,6 +7,8 @@ from socket import *
 from datetime import datetime
 from argparse import ArgumentParser
 
+MAX_PACKET_SIZE = 1024
+
 def main():
     # main function
     (trackingServer, trackingPort, localPort, sharedDir) = parse_args()
